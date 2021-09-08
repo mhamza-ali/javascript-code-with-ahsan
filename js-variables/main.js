9799232dc856a91d2cf834cc820620599a4caec9
mhamza-ali <hamza4841333@gmail.com>
2021-09-08 13:28:57 +0500

    js variables

---
+++ js-variables/main.js
@@ -0,0 +1,24 @@
+//console.log("Helo World");
+
+// var <variableName> = <vaue>;   =>how to declare variable 
+// other languages
+// int age = 10;
+// string name = 'hamza';
+// float percentage= 87.3;
+
+// In javascropt
+// var age = 10;
+// var name = 'hamza';
+// var percentage = 86.4;
+
+// var <variableName> = <vaue>;
+// let <variableName> = <vaue>;
+// const <variableName> = <vaue>;
+
+// const PI = 3.142;
+// console.log(PI);
+
+// let pi = 22/7;
+// console.log(pi);
+
+// data types in javascript
