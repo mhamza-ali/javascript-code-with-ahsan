b165be6761d1d33a2d2b839afe1984ad4e4ced1c
mhamza-ali <hamza4841333@gmail.com>
2021-09-08 14:32:44 +0500

    basics

---
+++ js-basics/main.js
@@ -0,0 +1,67 @@
+//console.log("Helo World");
+
+// var <variableName> = <vaue>;   =>how to declare variable 
+// other languages
+// int age = 10;
+// string name = 'hamza';
+// float percentage= 87.3;
+
+// In javascropt
+// var age = 10;
+// var name = 'hamza';
+// var percentage = 86.4;
+
+// var <variableName> = <vaue>;
+// let <variableName> = <vaue>;
+// const <variableName> = <vaue>;
+
+// const PI = 3.142;
+// console.log(PI);
+
+// let pi = 22/7;
+// console.log(pi);
+
+// data types in javascript
+// Boolean type
+
+// camelCase notation, snake_case is_today_saturday
+//const isTodaySaturday = true;
+
+// Null type
+// if car is started the value will be 0 - 120
+// if the car is not started the value is `null`
+//const carSpeedInKm = null;
+
+// Undefined type
+// let myVariable;
+// console.log(myVariable);
+
+// Number type
+const myAge = 20;
+console.log(myAge);
+
+// string type
+const myName = 'Hamza';
+console.log(myName)
+
+// typeof to check var type
+console.log(typeof myAge);
+
+let myVar = 22;
+console.log(typeof myVar);
+myVar = "It is string";
+console.log(myVar);
+console.log(typeof myVar);
+
+// operators
+
+// = assignment, + add, - subtract, 8 multiply, / divide
+// const num1 = 15;
+// const num2 = 23;
+// const result = num1 + num2
+// console.log(result);
+
+const num1 = Number(prompt("Enter first number"));
+const num2 = Number(prompt("Enter your second number"));
+const result = num1 + num2
+console.log(result);
